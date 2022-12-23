9c465430a01d6ed0123cea5282084064afd8deab
Devina02 <devina.fitriapngs@gmail.com>
2022-12-23 15:46:08 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE5_094_KELOMPOK5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
